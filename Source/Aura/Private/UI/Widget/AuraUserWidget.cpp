2cf5ea45f82426d02fdf8c6b07731c96e061670a
Maple <2931527112@qq.com>
2025-05-26 21:10:50 +0800

    2025.05.26 晚上 -> P030

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright by MapleNaaa
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSetup();
+}
